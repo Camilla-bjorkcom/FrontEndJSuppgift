f059e8740ae0d4d34ff47578e1d721ce1f79b37b
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2023-10-28 19:13:30 +0200

    browser router routes route done

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Contact from './views/Contact';
 import Home from './views/Home';
 import News from './views/News';
 import NotFound from './views/NotFound';
-import NewsDetails from './views/NewsDetails'
+import NewsDetails from './views/NewsDetails';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="wrapper">
-      <Home />
-      <Contact />
-      <News />
-      <NewsDetails />
-      <NotFound />
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='news' element={<News />} />
+      <Route path='newsdetails' element={<NewsDetails />} />
+      <Route path='notfound404' element={<NotFound />} />
+    </Routes>
   );
 }
+
+
+<div className="wrapper">
+
+</div>
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './views/Home';
-import Contact from './views/Contact';
-import NotFound from './views/NotFound';
-import News from './views/News';
-import NewsDetails from './views/NewsDetails';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,13 +11,6 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/' element={<Contact />} />
-        <Route path='/' element={<News />} />
-        <Route path='/' element={<NewsDetails />} />
-        <Route path='/' element={<NotFound />} />
-      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
