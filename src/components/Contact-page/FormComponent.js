8d19ed12df008d74317680887ecab45da5317414
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2023-11-09 20:21:17 +0100

    useContext,m.m.

--- src/components/Contact-page/FormComponent.js
+++ src/components/Contact-page/FormComponent.js
@@ -1,72 +1,92 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ButtonYellow from '../Generics/ButtonYellow'
+import { ErrorMessage, useFormik, yupToFormErrors } from 'formik'
+import * as Yup from 'yup'
 
 
 const FormComponent = () => {
+    const emailRegex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('')
     const [returnMessage, setReturnMessage] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setErrorMessage('');
+    const form = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+        },
 
-        const message = {name, email, message};
-        const json = JSON.stringify(message);
+        validationSchema: Yup.object( {
+            name: Yup.string()
+            .required("Förnamn måste anges")
+            .min(2, "Förnamnet måste bestå av minst 2 tecken"),
+            email: Yup.string()
+            .required("E-postadress måste anges")
+            .matches(emailRegex, "E-postadressen måste vara giltig"),
+        }),
 
-        const result = await fetch ('https://win23-assignment.azurewebsites.net/api/contactform', {
-            method: 'post',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: json
-        })
+        onSubmit: async (values) => {
+            const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+                method: 'post',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            })
 
-        switch (result.status) {
-            case 200:
-                setReturnMessage('meddelandet togs emot');
-                break;
-            case 400:
-                setErrorMessage(`något har gått fel. Felmeddelandet är ${await result.text()}`)
-                break;
-            case 409:
-                setErrorMessage(`något har gått fel. Felmeddelandet är ${await result.text()}`)
-                break;
+            switch (result.status) {
+                case 200:
+                    clearForm();
+                    setReturnMessage('Formuläret har skickats');
+                    break;
+                case 400:
+                    setErrorMessage(`något har gått fel. Felmeddelandet är: ${await result.text()}`)
+                    break;
+                case 409:
+                    setErrorMessage(`något har gått fel. Felmeddelandet är: ${await result.text()}`)
+                    break;
+            }
+
         }
+    })
+
+
+    const clearForm = () => {
+        setName('');
+        setEmail('');
+        setMessage('');
     }
-   
 
-  return (
-    <div className="form-wrapper">
-    <form id="form" onSubmit={handleSubmit} noValidate>
-        <p className="errorMessage">{errorMessage}</p>
-        <p className="returnMessage">{returnMessage}</p>
-        <div className="mb-3">
-            <label for="name">
-            </label>
-            <input id="name" type="text" name="Name" placeholder="Name*" className="form-input p-2" tabindex="1" value={name} onChange={(e) => setName(e.target.value)} />
+    return (
+        <div className="form-wrapper">
+            <form id="form" onSubmit={form.handleSubmit} noValidate>
+                <p className="errorMessage">{errorMessage}</p>
+                <p className="returnMessage">{returnMessage}</p>
+
+                <div className="mb-3">
+                    <label for="name">
+                    {form.errors.name && form.touched.name ? form.errors.name: ''}</label>
+                    <input id="name" type="text" name="name" placeholder="Name*" className="form-input p-2" tabindex="1" value={form.values.name} onChange={form.handleChange} onBlur={form.handleBlur} />
+                </div>
+
+                <div className="mb-3">
+                    <label for="email">{form.errors.email && form.touched.email ? form.errors.email: ''}
+                    </label>
+                    <input id="email" type="email" name="email" placeholder="Email*" className="form-input p-2" tabindex="2" value={form.values.email} onChange={form.handleChange} onBlur={form.handleBlur} />
+                </div>
+
+                <div className="mb-3">
+                    <label for="message">{form.errors.message && form.touched.message ? form.errors.message: ''}
+                    </label>
+                    <textarea name="message" id="message" placeholder="Your Message*" className="form-input p-2" tabindex="3" value={form.values.message} onChange={form.handleChange} onBlur={form.handleBlur} ></textarea>
+                </div>
+
+                {/* <ButtonYellow text="Send Message" url="#"/> */}
+                <a href="#"><button class="btn-yellow btn btn-warning form-input" type="submit">Send Message<i class="fa-regular fa-arrow-up-right"></i></button></a>
+            </form>
         </div>
-
-        <div className="mb-3">
-            <label for="email">
-            </label>
-            <input id="email" type="email" name="Email" placeholder="Email*" className="form-input p-2" tabindex="2" value={email} onChange={(e) => setEmail(e.target.value)}  />
-        </div>
-
-        <div className="mb-3">
-            <label for="message">
-            </label>
-            <textarea name="message" id="message" placeholder="Your Message*" className="form-input p-2" tabindex="3"value={message} onChange={(e) =>setMessage(e.target.value)} ></textarea>
-        </div>
-
-        {/* <ButtonYellow text="Send Message" url="#"/> */}
-        <a href="#"><button class="btn-yellow btn btn-warning form-input" type="submit">Send Message<i class="fa-regular fa-arrow-up-right"></i></button></a>
-    </form>
-</div>
-  )
+    )
 }
 
 export default FormComponent
